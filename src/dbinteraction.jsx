ab1f8cd0c5bae172933852ba24d8de69f871a3a6
SYL-Rutvik <rshiyal632@rku.ac.in>
2026-02-27 13:18:47 +0530

    sample

---
+++ src/dbinteraction.jsx
@@ -0,0 +1,71 @@
+import { use, useState } from "react";
+import axios from "axios";
+
+export default function DBinteraction() {
+    const [user, setUser] = useState();
+    const [name,setName] = useState("");
+    const [run,setRun] = useState("");
+    const [country,setCountry] = useState("");
+    const getuser = async () => {
+        try {
+            const res = await axios.get("http://localhost:3000/users");
+            setUser(res.data);
+        }
+        catch(err) {
+            console.log(err);
+        }
+        
+    }
+    const addUser = async () => {
+        try {
+            const res = await axios.post("http://localhost:3000/users",{
+                name,
+                run,
+                country
+            });
+            setUser(res.data)
+
+            
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+    const updateUser = async () => {
+        try {
+            
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
+        
+
+    return(
+        <>
+            <input
+                type="text"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)} />
+            <input
+                type="text"
+                placeholder="Run"
+                onChange={(e)=>setRun(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Country"
+                onChange={(e)=>setCountry(e.target.value)}
+            />
+            <button onClick={addUser}>Add User</button>
+    <button onClick={getuser}>Get User</button>
+        <ul>
+            {user && user.map((u)=> <li key={u.id}>
+                {u.name}
+                {u.run}
+                {u.country}
+            </li>)}
+        </ul>
+        </>
+    )
+}
